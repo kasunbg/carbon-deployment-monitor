9a0682231420e323f1e63f337850fd650937e2d0
Sumedha Kodithuwakku <sumedhassk@gmail.com>
2016-07-03 20:23:28 +0530

    Removing unused import

--- deployment-monitor-core/src/main/java/org/wso2/deployment/monitor/core/model/GlobalConfig.java
+++ deployment-monitor-core/src/main/java/org/wso2/deployment/monitor/core/model/GlobalConfig.java
@@ -17,8 +17,6 @@
 */
 package org.wso2.deployment.monitor.core.model;
 
-import org.wso2.deployment.monitor.core.MonitoringConstants;
-import org.wso2.deployment.monitor.core.scheduler.utils.SchedulerConstants;
 
 /**
  * todo
