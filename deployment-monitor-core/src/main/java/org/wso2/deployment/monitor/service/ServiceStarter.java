92576b90b18ae63aac42a9c24a3c93caf3784b79
Sumedha Kodithuwakku <sumedhassk@gmail.com>
2016-07-15 17:24:08 +0530

    Ability to define a custom MSF4J Service Implementation

--- deployment-monitor-core/src/main/java/org/wso2/deployment/monitor/service/ServiceStarter.java
+++ deployment-monitor-core/src/main/java/org/wso2/deployment/monitor/service/ServiceStarter.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.wso2.deployment.monitor.core.model.ServiceConfig;
 import org.wso2.msf4j.MicroservicesRunner;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * This will start a MSF4J based service to control the Deployment Monitor
  */
@@ -13,9 +16,29 @@ public class ServiceStarter {
     private static final Logger logger = LoggerFactory.getLogger(ServiceStarter.class);
 
     public static void startService(ServiceConfig serviceConfig) {
+
         if (serviceConfig.isEnabled()) {
             logger.info("Starting Deployment Monitor Service in port : " + serviceConfig.getPort());
-            new MicroservicesRunner(serviceConfig.getPort()).deploy(new DeploymentMonitorService()).start();
+            try {
+                Class<?> taskClass = Class.forName(serviceConfig.getServiceClass());
+                Constructor[] constructors = taskClass.getDeclaredConstructors();
+                Constructor constructor = null;
+                for (Constructor tempConstructor : constructors) {
+                    constructor = tempConstructor;
+                    if (constructor.getGenericParameterTypes().length == 0) {
+                        break;
+                    }
+                }
+                if (constructor != null) {
+                    new MicroservicesRunner(serviceConfig.getPort()).deploy(constructor.newInstance()).start();
+                } else {
+                    logger.error("Error occurred while starting the service : " + serviceConfig.getServiceClass()
+                            + " Constructor was not found.");
+                }
+            } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+                logger.error("Error occurred while starting the service", e);
+            }
+
         } else {
             logger.warn("Deployment Monitor service is disabled");
         }
